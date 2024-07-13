6405aed8f2cebf52bbedb769ece5a8620afb9904
Duck_fox <2660759310@qq.com>
2024-07-13 10:09:53 +0800

    更新2.75，更新ReflectionUtils的getClassLoader方法用来加载外部Jar

--- src/main/java/com/duckfox/duckapi/utils/ReflectionUtil.java
+++ src/main/java/com/duckfox/duckapi/utils/ReflectionUtil.java
@@ -1,5 +1,11 @@
 package com.duckfox.duckapi.utils;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+
 public class ReflectionUtil {
     private ReflectionUtil() {
     }
@@ -28,5 +34,16 @@ public class ReflectionUtil {
         } catch (NoSuchFieldException | IllegalAccessException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public static URLClassLoader getClassLoader(String url) throws NoSuchMethodException, MalformedURLException, InvocationTargetException, IllegalAccessException {
+        URLClassLoader classLoader = new URLClassLoader(new URL[]{}, ClassLoader.getSystemClassLoader());
+        Method method = URLClassLoader.class.getDeclaredMethod("addURL", URL.class);
+        if (!method.isAccessible()) {
+            method.setAccessible(true);
+        }
+        URL url1 = new URL("file:" + url);
+        method.invoke(classLoader, url1);
+        return classLoader;
     }
 }
